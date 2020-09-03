8fd1f3c9e283a0b841639b7f47d5363cfc0716d0
moalaakh7 <Alaa.Alkhwam@technolead.com>
2020-09-04 02:36:31 +0300

    Add Store

--- AdminPanel/View/Store/AddStore.cs
+++ AdminPanel/View/Store/AddStore.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdminPanel.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Models;
+using Library;
 
 namespace AdminPanel.View.Store
 {
@@ -15,6 +18,21 @@ namespace AdminPanel.View.Store
         public AddStore()
         {
             InitializeComponent();
+        }
+
+        private void btnAddStore_Click(object sender, EventArgs e)
+        {
+            if (!txtName.CheckNull() && !txtPhone.CheckNull() && txtLocation.CheckNull())
+            {
+                Models.Store store = new Models.Store
+                {
+                    Name = txtName.Text,
+                    PhoneNumber = txtPhone.Text,
+                    Location = txtLocation.Text
+                };
+                store = CallAPI.PostObjectAndGetObject<Models.Store, Models.Store>("AddNewStore");
+                MessageBox.Show("", "");
+            }
         }
     }
 }
---
+++ Models/Store.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Location { get; set; }
+    }
+}
